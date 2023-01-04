4f71bc8df9ad4a8438f972c27258c569f253a50e
Juan Estrella <jcestrella@uce.edu.ec>
2023-01-03 19:46:18 -0500

    postgres conexion

---
+++ src/main/java/com/example/demo/ProyectoPaU2JcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU2JcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU2JcApplication.class, args);
+	}
+
+}
