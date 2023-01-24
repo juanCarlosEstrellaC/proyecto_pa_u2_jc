798ca0db90a7fb9629e7ae9ecbbf133c5ddc4767
Juan Estrella <jcestrella@uce.edu.ec>
2023-01-24 12:48:42 -0500

    taller 22 con apuntes de cómo usar cascade, según lo que entendí.

--- src/main/java/com/example/demo/ProyectoPaU2JcApplication.java
+++ src/main/java/com/example/demo/ProyectoPaU2JcApplication.java
@@ -44,42 +44,45 @@ public class ProyectoPaU2JcApplication implements CommandLineRunner{
 		
 		
 		
-//	// Ejemplo 1: ingreso un ciudadano, con empleado:
+//	// Ejemplo 1: ingreso unicamente ciudadano, con empleado en cascada:
 //		
-//		// 1ro. Creo un ciudadano
+//		// 1. Creo un ciudadano
 //		Ciudadano ciu1 = new Ciudadano();
-//		ciu1.setNombre("AAA");
-//		ciu1.setApellido("Roman");
+//		ciu1.setNombre("Julian");
+//		ciu1.setApellido("Alvarez");
 //			
-//		// 2do. Creo un empleado
+//			// 1.2. Creo un empleado
 //			Empleado empl1 = new Empleado();
-//			empl1.setSalario(new BigDecimal(20));
+//			empl1.setSalario(new BigDecimal(4000));
 //			empl1.setFechaIngreso(LocalDateTime.now());
 //			empl1.setCiudadano(ciu1);
 //			
-//		// 3ro. Ingreso el empleado con el creado anteriormente.
+//		// y seteo el empleado con el creado anteriormente (sigue siendo el paso 1).
 //		ciu1.setEmpleado(empl1);		
 //		
+//		// 2. Inserto en la DB solo el ciudadano, y como está en cascada, se ingresará también el empleado,
+//		//    sin necesidad de llamar a un "this.empleadoService.guardar(empl1)".
 //		this.ciudadanoService.guardar(ciu1);
 		
-//	// Ejemplo 2: ingreso un empleado, con ciudadano 2:
-//		
-//		// 1ro. Creo un Empleado
-//		Empleado empl2 = new Empleado();
-//		empl2.setSalario(new BigDecimal(7));
-//		empl2.setFechaIngreso(LocalDateTime.now());
-//			
-//		//2do. Creo un ciudadano
-//			Ciudadano ciu2 = new Ciudadano();
-//			ciu2.setNombre("dfhgjk");
-//			ciu2.setApellido("abc");
-//			ciu2.setEmpleado(empl2); //**************????????????? va, creo q si
-//			
-//		// 3ro. Ingreso el ciudadano con el creado anteriormente.
-//		empl2.setCiudadano(ciu2);
-//		
-//		// 4to: Guardo el empleado
-//		this.empleadoService.guardar(empl2);
+	// Ejemplo 2: ingreso unicamente empleado, con ciudadano en cascada:
+		
+		// 1. Creo un Empleado
+		Empleado empl2 = new Empleado();
+		empl2.setSalario(new BigDecimal(7));
+		empl2.setFechaIngreso(LocalDateTime.now());
+			
+			//1.2. Creo un ciudadano
+			Ciudadano ciu2 = new Ciudadano();
+			ciu2.setNombre("dfhgjk");
+			ciu2.setApellido("abc");
+			ciu2.setEmpleado(empl2); 
+			
+		// y Seteo el ciudadano con el creado anteriormente (sigue siendo el paso 1).
+		empl2.setCiudadano(ciu2);
+		
+		// 2. Inserto en la DB solo el empleado, y como está en cascada, se ingresará también el ciudadano,
+		//    sin necesidad de llamar a un "this.ciudadanoService.guardar(ciu1)".
+		this.empleadoService.guardar(empl2);
 		
 	}
 	
